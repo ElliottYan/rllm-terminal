24f69b9b285f3fc6eca573b347337acd2d97faf6
ElliottYan <elliottyan37@gmail.com>
2026-03-29 22:35:29 +0800

    update script

---
+++ exp_scripts/pjlab_env.sh
@@ -0,0 +1,27 @@
+set -x
+
+# pip3 install --upgrade pip -i http://pypi.sankuai.com/simple/ --trusted-host pypi.sankuai.com
+# 
+# pip3 install --upgrade setuptools>=64.0.0 -i http://pypi.sankuai.com/simple/ --trusted-host pypi.sankuai.com
+
+# LOCAL_DIR=/mnt/dolphinfs/hdd_pool/docker/user/hadoop-aipnlp/FMG/yanjianhao03/
+LOCAL_DIR=/mnt/petrelfs/yanjianhao/rllm-terminal
+source $LOCAL_DIR/setup.sh
+
+# pip3 install hope==3.6.8 -i http://pypi.sankuai.com/simple/ --trusted-host pypi.sankuai.com --upgrade
+export VLLM_ATTENTION_BACKEND=FLASH_ATTN
+export PYTORCH_CUDA_ALLOC_CONF="expandable_segments:False"
+export VLLM_USE_V1=1
+export VLLM_ALLOW_LONG_MAX_MODEL_LEN=1
+export VLLM_ENGINE_ITERATION_TIMEOUT_S=100000000000
+
+# pip3 install hope==3.6.8 -i http://pypi.sankuai.com/simple/ --trusted-host pypi.sankuai.com --upgrade
+# 
+# MODEL_PATH=/mnt/dolphinfs/ssd_pool/docker/user/hadoop-nlp-sh02/hadoop-aipnlp/FMG/yinyongjing/hfmodels/DeepSeek-R1-Distill-Qwen-1.5B
+# MODEL_PATH=/mnt/dolphinfs/hdd_pool/docker/user/hadoop-aipnlp/FMG/yanjianhao03/huggingface.co/Qwen/Qwen3-4B-Instruct-2507
+# MODEL_PATH=/mnt/dolphinfs/hdd_pool/docker/user/hadoop-aipnlp/FMG/yanjianhao03//Qwen3-8B
+
+# LOCAL_PWD=/mnt/dolphinfs/ssd_pool/docker/user/hadoop-nlp-sh02/hadoop-aipnlp/FMG/yinyongjing
+MODEL_PATH=/mnt/petrelfs/share_data/quxiaoye/models/Qwen3-8B/
+LOCAL_PWD=$LOCAL_DIR
+# LOCAL_PWD=/mnt/dolphinfs/hdd_pool/docker/user/hadoop-aipnlp/FMG/yanjianhao03/rllm-terminal/
